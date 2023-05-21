7cc86ac790b7939c7b25bc58c86c3328cd8a5ab1
Karen <kuru_gutierrez@hotmail.com>
2023-05-21 20:22:38 -0300

    Documentación del controlador Servicio de Paseos

--- tp2_grupo16/src/main/java/ar/edu/unju/fi/controller/ServicioDePaseosController.java
+++ tp2_grupo16/src/main/java/ar/edu/unju/fi/controller/ServicioDePaseosController.java
@@ -19,12 +19,14 @@ public class ServicioDePaseosController {
 	boolean edicion = false;
 	Servicio servicioEncontrado;
 
+	//Controlador para mostrar la página de lista de servicios de paseos.
 	@GetMapping("/listado")
 	public String getServicioDePaseosPage(Model model) {
 		model.addAttribute("servicios", listaServicios.getServicios());
 		return "sdpaseos";
 	}
 	
+	//Controlador para mostrar la página de nuevo servicio.
 	@GetMapping("/nuevo")
 	public String getListadoDeServiciosPage(Model model) {
 		edicion = false;
@@ -35,6 +37,7 @@ public class ServicioDePaseosController {
 		return "nuevo_servicio";
 	}
 	
+	//Controlador para procesar el formulario de nuevo servicio y guardarlo en la lista de servicios.
 	@PostMapping("/guardar")
 	public ModelAndView getGuardarServiciopage(@ModelAttribute("servicio")Servicio servicio) {
 		ModelAndView modelView = new ModelAndView("sdpaseos");
@@ -43,6 +46,7 @@ public class ServicioDePaseosController {
 		return modelView;
 	}
 	
+	//Controlador para mostrar la página de modificación de un servicio existente.
 	@GetMapping("/modificar/{nombre}")
 	public String getModificarServicioPage(Model model, @PathVariable(value="nombre")String nombre) {
 		servicioEncontrado = null;
@@ -61,6 +65,7 @@ public class ServicioDePaseosController {
 		return "nuevo_servicio";
 	}
 	
+	//Controlador para procesar la modificación de un servicio existente.
 	@PostMapping("/modificar")
 	public String modificarServicio(@ModelAttribute("servicio")Servicio servicio) {
 		for (Servicio servi : listaServicios.getServicios()) {
@@ -74,6 +79,7 @@ public class ServicioDePaseosController {
 		return "redirect:/servicios/listado";
 	}
 	
+	//Controlador para eliminar un servicio.
 	@GetMapping("/eliminar/{nombre}")
 	public String eliminarServicio(@PathVariable(value="nombre")String nombre) {
 		for (Servicio servi : listaServicios.getServicios()) {
