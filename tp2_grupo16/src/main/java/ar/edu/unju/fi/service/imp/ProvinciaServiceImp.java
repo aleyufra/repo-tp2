7814459914b75415d39f26e8d724584f6019ef66
aleyufra <alejandromaryufra@gmail.com>
2023-06-24 21:38:04 -0300

    se mapea la clase sucursales

--- tp2_grupo16/src/main/java/ar/edu/unju/fi/service/imp/ProvinciaServiceImp.java
+++ tp2_grupo16/src/main/java/ar/edu/unju/fi/service/imp/ProvinciaServiceImp.java
@@ -1,5 +1,36 @@
 package ar.edu.unju.fi.service.imp;
 
-public class ProvinciaServiceImp {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ar.edu.unju.fi.entity.Provincia;
+import ar.edu.unju.fi.repository.IProvinciaRepository;
+import ar.edu.unju.fi.service.IProvinciaService;
+
+@Service("provinciaServiceImp")
+public class ProvinciaServiceImp implements IProvinciaService {
+	
+	@Autowired
+	private IProvinciaRepository provinciaRepository;
+	
+	@Autowired
+	private Provincia provincia;
+	
+	@Override
+	public List<Provincia> getProvincias() {
+		return (List<Provincia>) provinciaRepository.findAll();
+	}
+
+	@Override
+	public void saveProvincia(Provincia provincia) {
+		provinciaRepository.save(provincia);
+	}
+
+	@Override
+	public Provincia getProvincia() {
+		return provincia;
+	}
 
 }
